3266e3684be08a00ed490baa8626601e16c62e98
Bruno Jurkovski <bjurkovski@gmail.com>
2025-08-23 11:05:59 +0100

    Bootstrap hugo

---
+++ devenv.nix
@@ -0,0 +1,30 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.hugo
+  ];
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
